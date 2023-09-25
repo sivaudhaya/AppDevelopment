7ee0d51cc2716be4a850428feae795a6c49c5c1e
sivaudhaya <104961138+sivaudhaya@users.noreply.github.com>
2023-09-25 16:23:00 +0530

    Add files via upload

---
+++ DAY7/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import Login from './Login/Loginjs';
+import Signup from './Signup/Signupjs';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './DashBoardPage/DashBoard';
+import OrderHistory from './DashBoardPage/OrderHistory';
+
+import ProductList from './DashBoardPage/ProductList';
+
+import Homepage from './HomePage/Homepage';
+import About from './About/About';
+import PrivacyPolicy from './DashBoardPage/Privacy';
+import TermsAndConditions from './DashBoardPage/Terms';
+import Appone from './Land/Land';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Homepage/>} />
+        <Route path="/dash" element={<Dashboard/>} />
+        <Route path="/order" element={<OrderHistory/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/privacy" element={<PrivacyPolicy/>} />
+        <Route path="/term" element={<TermsAndConditions/>} />
+        <Route path="/" element={<Appone/>} />
+   
+      
+        <Route path="/productlist" element={<ProductList/>} />
+     
+      </Routes>
+    </Router>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;

7828f6e3d34930fdcb07387eeee6d8e58897a98c
sivaudhaya <104961138+sivaudhaya@users.noreply.github.com>
2023-10-05 16:15:19 +0530

    Add files via upload

---
+++ DAY14/src/main/java/com/example/demo/Customtshirtprinting1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Customtshirtprinting1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Customtshirtprinting1Application.class, args);
+	}
+
+}
